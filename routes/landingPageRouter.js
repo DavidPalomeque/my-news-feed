350f5b261e301079109116119126cbc96433dd55
unknown <davidpalomeque2001@gmail.com>
2021-02-09 20:38:20 -0300

    connecting with db and api

--- routes/landingPageRouter.js
+++ routes/landingPageRouter.js
@@ -1,7 +1,17 @@
 const router = require("express").Router()
 const landingPageCtrl = require("../controllers/landingPageController")
+const express = require("express")
+const app = express()
+const https = require('https');
+http = require('http')
+const request = require("request")
 
 router.get("/" , (req , res) => {
+    request(`https://newsapi.org/v2/everything?q=tesla&from=2021-01-09&sortBy=publishedAt&apiKey=${process.env.NEWS_API_KEY}`, function (error, response, body) {
+        console.error('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body); // Print the HTML for the Google homepage.
+    });
     res.render("landingpage/landingpage")
 })
 
