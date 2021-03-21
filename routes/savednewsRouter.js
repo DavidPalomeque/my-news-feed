d5fb8929d59c1c838bdc988607f64ab8e60e65d0
unknown <davidpalomeque2001@gmail.com>
2021-03-21 16:48:02 -0300

    add: 'save new' route in router

--- routes/savednewsRouter.js
+++ routes/savednewsRouter.js
@@ -3,6 +3,7 @@ const savednewsCtrl = require("../controllers/savednewsController")
 const {ensureAuthenticated} = require("../config/auth")
 
 router.get("/savednews/:user_id" , ensureAuthenticated , savednewsCtrl.showList)
+router.post("/savednews/add" , ensureAuthenticated , savednewsCtrl.addToList)
 router.get("/savednews/delete/:savednew_id" , ensureAuthenticated , savednewsCtrl.deleteFromList)
 // router.get("/shopcart/clean/:user_id" , ensureAuthenticated , shopcartCtrl.cleanShopcart)
 
